0744b3df21949706f6c437062ba3a7762072c584
robisoncrisbrito <robison.brito@gmail.com>
2025-03-15 13:20:49 -0300

    com Internacioalização e literais

--- app/src/main/java/br/edu/utfpr/calculaimc_kotlin/MainActivity.kt
+++ app/src/main/java/br/edu/utfpr/calculaimc_kotlin/MainActivity.kt
@@ -1,20 +1,77 @@
 package br.edu.utfpr.calculaimc_kotlin
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import java.text.DecimalFormat
+import java.text.NumberFormat
+import java.util.Locale
+import kotlin.math.pow
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var etPeso : EditText
+    private lateinit var etAltura : EditText
+    private lateinit var tvResultado : TextView
+    private lateinit var btCalcular : Button
+    private lateinit var btLimpar : Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        etPeso = findViewById( R.id.etPeso )
+        etAltura = findViewById( R.id.etAltura )
+        tvResultado = findViewById( R.id.tvResultado )
+        btCalcular = findViewById( R.id.btCalcular )
+        btLimpar = findViewById( R.id.btLimpar )
+
+        btCalcular.setOnClickListener {
+            btCalcularOnClick()
         }
+
+        btLimpar.setOnClickListener {
+            btLimparOnClick()
+        }
+
+    }
+
+    private fun btLimparOnClick() {
+        etPeso.setText( "" )
+        etAltura.setText( "" )
+        tvResultado.text = getString( R.string.zeros )
+        etPeso.requestFocus()
+    }
+
+    private fun btCalcularOnClick() {
+
+        if ( etPeso.text.toString().isEmpty() ) {
+            etPeso.error = getString(R.string.error_peso)
+            return
+        }
+
+        if ( etAltura.text.toString().isEmpty() ) {
+            etAltura.error = getString(R.string.error_altua)
+            return
+        }
+
+        val peso = etPeso.text.toString().toDouble()
+        val altura = etAltura.text.toString().toDouble()
+
+        var imc : Double
+
+        if ( Locale.getDefault().language.equals( "en" ) ) {
+            imc = 703 * ( peso / altura.pow( 2 ) )
+            val nf = NumberFormat.getNumberInstance( Locale.US )
+            val df = nf as DecimalFormat
+            tvResultado.text= df.format( imc )
+        } else {
+            imc = peso / altura.pow( 2 )
+            val df = DecimalFormat( "0.0" )
+            tvResultado.text= df.format( imc )
+        }
+
     }
 }
